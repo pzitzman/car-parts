f24d4a261ae7220c57db7e41acac15fa765f6952
Dennis Aust <dennis.aust@xl2.de>
2026-04-27 11:55:13 +0200

    defined Enity in backend

--- backend/Data/Entity/CarPart.cs
+++ backend/Data/Entity/CarPart.cs
@@ -1,2 +1,32 @@
-namespace backend.Data.Entity;
-public class CarPart {/*TODO */}
+using System;
+using DnsClient.Protocol;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Data.Entity
+{
+    public class CarParts
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement("machineId")]
+        public string MachineId { get; set; } = string.Empty;
+
+        [BsonElement("partNumber")]
+        public string PartNumber { get; set; } = string.Empty;
+
+        [BsonElement("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [BsonElement("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonElement("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
