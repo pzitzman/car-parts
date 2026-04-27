8b090f9fb3eda9507930acff3adf6e7605704846
Dennis Aust <dennis.aust@xl2.de>
2026-04-27 13:45:59 +0200

    add mongo collection and interface for car parts

--- backend/Data/Mongo/CarPartCollection.cs
+++ backend/Data/Mongo/CarPartCollection.cs
@@ -1,2 +1,23 @@
-namespace backend.Data.Mongo;
-public class CarPartCollection {/*TODO */}
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using backend.Data.Entity;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace backend.Data.Mongo
+{
+    public class CarPartCollection : ICarPartCollection
+    {
+        private readonly IMongoCollection<CarPart> _carPartsCollection;
+
+        public CarPartCollection(IOptions<DocumentDbSettings> documentDbSettings)
+        {
+            // Initialize MongoClient, get DB and bind CarPart
+            var mongoClient = new MongoClient(documentDbSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(documentDbSettings.Value.DatabaseName);
+            _carPartsCollection = mongoDatabase.GetCollection<CarPart>(
+                documentDbSettings.Value.CollectionName
+            );
+        }
+    }
+}
