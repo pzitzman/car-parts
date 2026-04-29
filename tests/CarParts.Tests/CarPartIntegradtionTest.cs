bd0921c334ce73bb9267400472c1f6f853a182c3
Dennis Aust <dennis.aust@xl2.de>
2026-04-29 15:55:58 +0200

    test: init xunit, add slnx and reconfigured gitignore

---
+++ tests/CarParts.Tests/CarPartIntegradtionTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace CarParts.Tests;
+
+public class CarPartIntegradtionTest(WebApplicationFactory<Program> factory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Fact]
+    public async Task ProofOfLive_ApiResponds()
+    {
+        var response = await _client.GetAsync("/api/carpart");
+        Assert.True(
+            response.IsSuccessStatusCode
+                || response.StatusCode == System.Net.HttpStatusCode.NotFound
+        );
+    }
+}
