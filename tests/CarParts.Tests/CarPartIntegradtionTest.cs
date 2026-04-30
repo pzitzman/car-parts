67fc5718a3218ac3cd4ef933486e894a23210fea
Dennis Aust <dennis.aust@xl2.de>
2026-04-30 10:37:57 +0200

    test: implement positive tests in xunit

--- tests/CarParts.Tests/CarPartIntegradtionTest.cs
+++ tests/CarParts.Tests/CarPartIntegradtionTest.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualBasic;
 
 namespace CarParts.Tests;
 
@@ -15,5 +19,50 @@ public class CarPartIntegradtionTest(WebApplicationFactory<Program> factory)
             response.IsSuccessStatusCode
                 || response.StatusCode == System.Net.HttpStatusCode.NotFound
         );
+    }
+
+    [Fact]
+    public async Task PositiveLoop()
+    {
+        //POST
+        var newPart = new
+        {
+            Name = "Ölfilter",
+            PartNumber = "123Oel-ABC",
+            Description = "Ein normaler Ölfilter",
+        };
+
+        var postResponse = await _client.PostAsJsonAsync("/api/carpart", newPart);
+        Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+
+        //Get id from response
+        var createdTempPart = await postResponse.Content.ReadFromJsonAsync<JsonDocument>();
+        Assert.NotNull(createdTempPart);
+        string testId =
+            createdTempPart.RootElement.GetProperty("id").GetString()
+            ?? throw new InvalidOperationException("The Api did not return an Id from Json");
+
+        //GET
+        var getResponse = await _client.GetAsync($"/api/carpart/{testId}");
+        Assert.Equal(HttpStatusCode.OK, getResponse.StatusCode);
+
+        //PUT
+        var partToUpdate = new
+        {
+            Name = "Luftfilter",
+            PartNumber = "9876F-ABC",
+            Description = "Ein normaler Luftfilter",
+        };
+
+        var putResponse = await _client.PutAsJsonAsync($"/api/carpart/{testId}", partToUpdate);
+        Assert.Equal(HttpStatusCode.NoContent, putResponse.StatusCode);
+
+        //DELETE
+        var deleteResponse = await _client.DeleteAsync($"/api/carpart/{testId}");
+        Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode);
+
+        //Verfiy deletion
+        var checkDeleted = await _client.GetAsync($"/api/carpart/{testId}");
+        Assert.Equal(HttpStatusCode.NotFound, checkDeleted.StatusCode);
     }
 }
